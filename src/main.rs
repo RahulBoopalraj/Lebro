69a4f249307e0510d4251d2020b2fcbbd51c2d0e
Rahul-CSEstudent <talktorahul.b@gmail.com>
2023-10-15 13:24:02 +0530

    everything setted up -- need to fix leptose_reactive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::ops::Deref;
+
+use wasm_bindgen::{self, JsCast, JsValue};
+use web_sys::{self, Element, Window, window, console, Event};
+use leptos_reactive::{self, create_signal, create_runtime, create_scope, Scope, SignalUpdate, SignalGet, create_effect};
+
 fn main() {
-    println!("Hello, world!");
+    mount(
+        E1::new("button")
+    )
+}
+
+fn mount(root: E1){
+    let window = window().unwrap();
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
+    body.append_child(&root).unwrap();
+}
+
+#[derive(Debug, Clone)]
+pub struct E1(Element);
+
+impl Deref for E1 {
+    type Target = Element;
+
+    fn deref(&self) -> &self::Target {
+        &self.0
+    }
+}
+
+impl E1 {
+    pub fn new(tag_name: &str) -> Self {
+        let window = window().unwrap();
+        let document = window.document().unwrap();
+        let e1 = document.create_element(tag_name).unwrap();
+        self(e1)
+    }
 }
